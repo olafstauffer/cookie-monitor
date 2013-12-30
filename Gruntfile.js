505bedff487f2d88c25087e3f77c15e11b1b2b5b
Olaf Stauffer <olaf.stauffer@gmail.com>
2013-12-30 06:51:55 +0100

    added minification and generation of a extension archive to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,6 +22,7 @@ module.exports = function (grunt) {
     };
 
     grunt.initConfig({
+        manifest: grunt.file.readJSON('app/manifest.json'),
         yeoman: yeomanConfig,
         'bower-install': {
             app: {
@@ -39,10 +40,133 @@ module.exports = function (grunt) {
                 '<%= yeoman.app %>/scripts/{,*/}*.js'
             ]
         },
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.tmp',
+                        '<%= yeoman.dist %>/*',
+                        '!<%= yeoman.dist %>/.git*'
+                    ]
+                }]
+            }
+        },
+        useminPrepare: {
+            html: [
+                '<%= yeoman.app %>/popup.html'
+            ],
+            options: {
+                dest: '<%= yeoman.dist %>/'
+            }
+        },
+        imagemin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= yeoman.app %>/images',
+                    src: '*.{png,jpg,jpeg}',
+                    dest: '<%= yeoman.dist %>/images'
+                }]
+            }
+        },
+        usemin: {
+            options: {
+                dirs: ['<%= yeoman.dist %>']
+            },
+            html: ['<%= yeoman.dist %>/{,*/}*.html'],
+            css: ['<%= yeoman.dist %>/styles/{,*/}*.css']
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    // removeComments: true
+                    collapseBooleanAttributes: true,
+                    removeCommentsFromCDATA: true,
+                    removeOptionalTags: true
+                    // collapseWhitespace: true
+                    // removeAttributeQuotes: true,
+                    // removeRedundantAttributes: true,
+                    // useShortDoctype: true,
+                    // removeEmptyAttributes: true,
+                    // removeOptionalTags: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= yeoman.app %>',
+                    src: '*.html',
+                    dest: '<%= yeoman.dist %>'
+                }]
+            }
+        },
+        // Allow the use of non-minsafe AngularJS files. Automatically makes it
+        // minsafe compatible so Uglify does not destroy the ng references
+        ngmin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '.tmp/concat/scripts',
+                    src: '*.js',
+                    dest: '.tmp/concat/scripts'
+                }]
+            }
+        },
+
+        // Put files not handled in other tasks here
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%= yeoman.app %>',
+                    dest: '<%= yeoman.dist %>',
+                    src: [
+                        'scripts/background.js'
+                    ]
+                }]
+            }
+        },
+        chromeManifest: {
+            dist: {
+                options: {
+                    buildnumber: false,
+                    background: {
+                        target:'scripts/background.js'
+                    }
+                },
+                src: '<%= yeoman.app %>',
+                dest: '<%= yeoman.dist %>'
+            }
+        },
+        compress: {
+            dist: {
+                options: {
+                    archive: 'package/chrome-extrension_cookie-monitor_v<%= manifest.version%>.zip'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'dist/',
+                    src: ['**'],
+                    dest: ''
+                }]
+            }
+        }
     });
 
     grunt.registerTask('build', [
-        'bower-install'
+        'bower-install',
+        'clean:dist',
+        'useminPrepare',
+        'concat',
+        'ngmin',
+        'uglify',
+        'cssmin',
+        'imagemin',
+        'htmlmin',
+        'usemin',
+        'chromeManifest',
+        'copy',
+        'compress'
     ]);
 
     grunt.registerTask('default', [
