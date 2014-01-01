7aee8e0f53143168d75906574702233b131f6528
Olaf Stauffer <olaf.stauffer@gmail.com>
2014-01-01 22:08:07 +0100

    removed angular protractor example from e2e tests

--- test/spec/popup_spec.js
+++ test/spec/popup_spec.js
@@ -96,17 +96,3 @@ describe('', function () {
 	});
 
 });
-
-
-xdescribe('angularjs homepage', function() {
-	it('should greet the named user', function() {
-		browser.get('http://www.angularjs.org');
-
-		element(by.model('yourName')).sendKeys('Julie');
-		var greeting = element(by.binding('yourName'));
-
-		// expect(greeting.getText()).toEqual('Hello Julie!');
-		expect(greeting.getText()).to.eventually.equal('Hello Julie!');
-	});
-
-});
